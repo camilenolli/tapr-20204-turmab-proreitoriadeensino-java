965e5bbdabd8bacd5d9131a2fdbb2ad3f1bf86d6
Camile Nolli <105087825+camilenolli@users.noreply.github.com>
2024-10-02 00:38:09 +0000

    WIP: criacao do projeto

---
+++ microservproreitoriadeensino/src/main/java/br/univille/microservproreitoriadeensino/MicroservproreitoriadeensinoApplication.java
@@ -0,0 +1,13 @@
+package br.univille.microservproreitoriadeensino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservproreitoriadeensinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservproreitoriadeensinoApplication.class, args);
+	}
+
+}
---
+++ microservproreitoriadeensino/src/test/java/br/univille/microservproreitoriadeensino/MicroservproreitoriadeensinoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.microservproreitoriadeensino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservproreitoriadeensinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
